8983e8c4e835a72644347bc64453d47ac31f7610
simon <simongizaw@gmail.com>
2022-10-04 18:21:08 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+      double itemPrice = 20;
+      double tax = (0.05 * itemPrice );
+
+      System.out.println("The price of your item is " + itemPrice + " and the price of tax is " + tax);
+
+    }
+}
